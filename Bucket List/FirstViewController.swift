213bbe601e0714cbabda0fe76e44431b33a6fe8f
Erin Michelle <emw337@drexel.edu>
2018-02-19 14:59:55 -0500

    table view setup to do and done

--- Bucket List/FirstViewController.swift
+++ Bucket List/FirstViewController.swift
@@ -8,7 +8,23 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var toDoList = ["Go ice skating", "See Christmas lights", "Go to the beach"]
+    
+    @IBOutlet weak var toDoTableView: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return(toDoList.count)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let toDoCell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "toDoCell")
+        toDoCell.textLabel?.text = toDoList[indexPath.row]
+        
+        return(toDoCell)
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- Bucket List/SecondViewController.swift
+++ Bucket List/SecondViewController.swift
@@ -8,7 +8,23 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITableViewDataSource {
+    
+    var doneList = ["Go to a pumpkin patch", "Visit cousins"]
+    
+    @IBOutlet weak var doneTableView: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return(doneList.count)
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let doneCell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "doneCell")
+        doneCell.textLabel?.text = doneList[indexPath.row]
+        
+        return(doneCell)
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
