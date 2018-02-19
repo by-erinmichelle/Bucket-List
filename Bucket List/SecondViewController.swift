76aaa2b25e1b0af35274ed43b90295635534f7d4
Erin Michelle <emw337@drexel.edu>
2018-02-19 13:46:17 -0500

    Initial Commit

---
+++ Bucket List/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Bucket List
+//
+//  Created by Erin Wiegman on 2/19/18.
+//  Copyright © 2018 Erin Wiegman. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
